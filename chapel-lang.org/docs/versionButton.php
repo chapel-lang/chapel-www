53c7fdde1ef1df91737e392cd75abe79c41b761a
Brad Chamberlain <bradcray@users.noreply.github.com>
2026-06-16 12:01:33 -0700

    Docs version button bump to prepare for unfreezing of 'main' for 2.10 dev
    ---
    Signed-off-by: Brad Chamberlain <bradcray@users.noreply.github.com>

--- chapel-lang.org/docs/versionButton.php
+++ chapel-lang.org/docs/versionButton.php
@@ -89,7 +89,7 @@ if (pagePath == "") {
 function dropSetup() {
   var currentRelease = "2.8"; // what does the public have?
   var stagedRelease = "2.9";  // is there a release staged but not yet public?
-  var nextRelease = "2.9";    // what's the next release? (on docs/main)
+  var nextRelease = "2.10";    // what's the next release? (on docs/main)
   var button = document.getElementById("versionButton");
   // Uses unicode down-pointing triangle
   var arrow = " &#9660;";
